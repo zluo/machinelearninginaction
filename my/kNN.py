b5219e734ba9579d15a28eb618c0ae443509b20e
zluo <zhanqing_luo@silanis.com>
2016-06-08 17:05:30 -0400

    read file

--- my/kNN.py
+++ my/kNN.py
@@ -24,6 +24,22 @@ def classifer0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
     
+
+def file2matrix(filename):
+    fr = open(filename)
+    lines = len(fr.readlines())
+    returnMat = zeros((lines, 3))
+    classLabelVector = []
+    fr = open(filename)
+    
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index +=1
+    return returnMat, classLabelVector
     
 
 import unittest
